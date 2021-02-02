358dc3dfc5a410b6cfc57019f61e7efb6db0c324
LeonardoLeiteMeira <42722914+LeonardoLeiteMeira@users.noreply.github.com>
2021-02-02 20:41:59 -0300

    Fix directorys

---
+++ src/pages/api/user.js
@@ -0,0 +1,33 @@
+import executeQuery from '../../database/db';
+
+async function HandlerUser(request, response){
+	
+	try{
+		// const results = await executeQuery({
+		// 	query: 'SELECT * FROM users'
+
+		// });
+		const results = [
+			{
+				name:"Leo",
+				lastName:"Leite"
+			},
+			{
+				name:"Lucca",
+				lastName:"Miranda"
+			},
+			{
+				name:"Savio",
+				lastName:"Cabral"
+			}
+		]
+			
+		return response.json(results);
+
+	}catch(error){
+		response.json({ message: error.message })
+	}
+	
+}
+
+export default HandlerUser;
