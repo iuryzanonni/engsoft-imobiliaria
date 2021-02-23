ffe17ed0ed31ae5a40381f21095af4a4666e81f8
Lucca Miranda <luccafmsilva@gmail.com>
2021-02-23 17:57:13 -0300

    remove console logs

--- src/database/index.js
+++ src/database/index.js
@@ -4,14 +4,12 @@ const knex = require("knex")(connectionDB.development);
 var cachedConnection;
 
 export const getDatabaseConnector = () => {
-  if (cachedConnection) {
-    console.log("Cached Connection");
-    return cachedConnection;
-  }
+    if (cachedConnection) {
+        return cachedConnection;
+    }
 
-  console.log("New Connection");
-  cachedConnection = knex;
-  return cachedConnection;
+    cachedConnection = knex;
+    return cachedConnection;
 };
 
 module.exports = getDatabaseConnector();
--- src/pages/api/user.js
+++ src/pages/api/user.js
@@ -1,19 +1,14 @@
-import executeQuery from '../../database';
+import executeQuery from "../../database";
 // import knex from 'knex';
-import database from '../../database';
+import database from "../../database";
 
-async function HandlerUser(request, response){
-	
-	try{		
-		const resp = await database.select('*').from('teste').where('id',2);
-		console.log(resp);
-			
-		return response.json(resp);
-
-	}catch(error){
-		response.json({ message: error.message })
-	}
-	
+async function HandlerUser(request, response) {
+    try {
+        const resp = await database.select("*").from("teste").where("id", 2);
+        return response.json(resp);
+    } catch (error) {
+        response.json({ message: error.message });
+    }
 }
 
 export default HandlerUser;
